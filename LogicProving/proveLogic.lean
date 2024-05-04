e8cf3ebeb415822a68fd751b90cb95d20ebc5351
Kaanak413 <thedosen48@gmail.com>
2024-05-05 00:07:07 +0300

    lemma7 proof with by_condatiction

--- LogicProving/proveLogic.lean
+++ LogicProving/proveLogic.lean
@@ -37,3 +37,103 @@ example (p q : Prop) : p ∨ q → q ∨ p := by
   cases h with
   | inl hp => apply Or.inr; exact hp
   | inr hq => apply Or.inl; exact hq
+
+
+theorem test (p q : Prop) (hp : p) (hq : q) : p ∧ q ∧ p := by
+  apply And.intro
+  case right =>
+    apply And.intro
+    case left => exact hq
+    case right => exact hp
+  case left => exact hp
+
+
+example : P ↔ P := by
+  apply Iff.refl
+  -- rfl
+
+example (y : Nat) : (fun x : Nat => 0) y = 0 := by
+    rfl
+
+example : ∀ a b c : Nat, a = b → a = c → c = b := by
+  intros
+  apply Eq.trans
+  apply Eq.symm
+  repeat assumption
+
+
+variable (A B C : Type)
+
+variable (PP QQ  : A → Prop)
+
+-- lemma two : (∀x : A , Px -> Q ) = (∃x : A , Px -> Q ) :=
+
+
+-- lemma three : ((∀x : A , PPx) ∧ (∀x : A , QQx) ) -> (∀x : A , PPx ∧  QQx ) := by
+--   intro hPP hA
+--   cases hPP with
+--   | intro h q =>
+--     cases q with
+--     | intro a x =>
+--       exists h
+
+
+  -- apply And.intro
+  -- case right =>
+  --   apply
+example : (∀ x : A, PP x ∧ QQ x)
+↔ (∀ x : A , PP x) ∧ (∀ x : A, QQ x) := by
+  constructor
+  intro h
+  constructor
+  intro a
+  have pq :PP a ∧ QQ a
+  apply h
+  cases pq with
+  | intro pa qa =>
+    exact pa
+  intro a
+  have pq :PP a ∧ QQ a
+  apply h
+  cases pq with
+  | intro pa qa =>
+    exact qa
+  intro h
+  cases h with
+  | intro hp qp =>
+    intro a
+    constructor
+    apply hp
+    apply qp
+
+
+#check em
+#check by_contradiction
+
+
+
+
+
+
+
+
+
+
+
+
+theorem lemma7 : ¬ (∀ x : A, PP x) ↔ ∃ x : A, ¬ PP x := by
+  constructor
+  intro h
+  apply by_contradiction
+  intro ne
+  apply h
+  intro a
+  apply by_contradiction
+  intro np
+  apply ne
+  exists a
+  intro h na
+  cases h with
+  | intro a np =>
+    apply np
+    apply na
